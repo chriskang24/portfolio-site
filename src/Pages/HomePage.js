e57bf4ee2a9d96a71632e70b0c07937062f95c8b
chriskang24 <christopherjkang24@gmail.com>
2021-04-10 19:14:11 -0400

    add init homepage description

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -2,8 +2,19 @@ import React from 'react';
 
 function HomePage() {
   return(
-    <div>
-      <hi>Homepage</hi>
+    <div className="HomePage">
+      <header className="hero">
+        <h1 className="hero-text">
+          Hi, I am
+          <span> Chris Kang </span>
+        </h1>
+      </header>
+      <p className="h-sub-text">
+        Welcome to my portfolio website! Please see the sidebar for more info!
+      </p>
+      <div className="icons">
+
+      </div>
     </div>
   )
 }
