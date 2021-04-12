8691e07b836c17f0d866b0ad7d5bd87abdc22cf0
chriskang24 <christopherjkang24@gmail.com>
2021-04-12 13:37:34 -0400

    add skills section to about page

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import Title from '../Components/title';
-import ImageSection from './ImageSection';
+import ImageSection from '../Components/ImageSection';
+import SkillsSection from '../Components/SkillsSection';
 
 export default function AboutPage() {
   return (
     <div className="AboutPage">
-      <Title title={'About Me'} span={'About Me 2'}/>
-      <ImageSection/>
+      <Title title={'About Me'} span={'About Me 2'} />
+      <ImageSection />
+      <Title title={'My Skills'} span={'My Skills 2'} />
+      <div className="skillsContainer">
+        <SkillsSection skill={'Javascript'} progress={'85%'} width={'85%'} />
+        <SkillsSection skill={'Typescript'} progress={'30%'} width={'30%'} />
+        <SkillsSection skill={'ReactJS'} progress={'75%'} width={'75%'} />
+        <SkillsSection skill={'NodeJS/ExpressJS'} progress={'80%'} width={'80%'} />
+        <SkillsSection skill={'Web Design'} progress={'70%'} width={'70%'} />
+        <SkillsSection skill={'Ruby/Rails'} progress={'50%'} width={'50%'} />
+      </div>
     </div>
   )
 }
