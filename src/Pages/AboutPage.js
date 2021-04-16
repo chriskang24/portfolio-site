55a0c7d19fa77ff6a46c7fc23a76f328278898c8
chriskang24 <christopherjkang24@gmail.com>
2021-04-16 15:32:47 -0400

    update skills desc

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -14,19 +14,19 @@ export default function AboutPage() {
       <ImageSection />
       <Title title={'My Skills:'} span={'My Skills'} />
       <div className="skillsContainer">
-        <SkillsSection skill={'Javascript'} progress={'85%'} width={'85%'} />
+        <SkillsSection skill={'JavaScript'} progress={'85%'} width={'85%'} />
         <SkillsSection skill={'NodeJS/ExpressJS'} progress={'80%'} width={'80%'} />
-        <SkillsSection skill={'ReactJS'} progress={'75%'} width={'75%'} />
-        <SkillsSection skill={'Web Design'} progress={'70%'} width={'70%'} />
+        <SkillsSection skill={'ReactJS/Redux'} progress={'75%'} width={'75%'} />
+        <SkillsSection skill={'jQuery/AJAX'} progress={'70%'} width={'70%'} />
         <SkillsSection skill={'Ruby/Rails'} progress={'50%'} width={'50%'} />
-        <SkillsSection skill={'Typescript'} progress={'30%'} width={'30%'} />
+        <SkillsSection skill={'TypeScript'} progress={'30%'} width={'30%'} />
       </div>
 
       <Title title={'Services:'} span={'Services'} />
       <div className="services-container">
         <ServiceSection image={design} title={'Web Development'} text={'Experienced in creating smooth website designs & applications.'} />
-        <ServiceSection image={intelligence} title={'Single Page Applications'} text={'Using ReactJS & other frameworks to create smooth single page applications.'} />
-        <ServiceSection image={gamedev} title={'Full Stack Developer'} text={'Front to Back-End developer with previous experience using ReactJS, Ruby/Rails, NodeJS/Express to service full-stack apps.'} />
+        <ServiceSection image={intelligence} title={'Single Page Applications'} text={'Using ReactJS, Redux, and other Front-end frameworks to create responsive single-page applications.'} />
+        <ServiceSection image={gamedev} title={'Full Stack Developer'} text={'Front to Back-End developer with previous experience using ReactJS, Ruby/Rails, NodeJS/Express to service full-stack applications.'} />
       </div>
     </div>
   )
