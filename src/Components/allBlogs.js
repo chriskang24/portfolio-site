aba2347ffab71b647b3361adedcdbee517e8e238
chriskang24 <christopherjkang24@gmail.com>
2021-04-15 20:09:48 -0400

    update blogs page

--- src/Components/allBlogs.js
+++ src/Components/allBlogs.js
@@ -1,7 +1,5 @@
-import blog1 from '../img/portImages/port1.jpg';
+import blog1 from '../img/portImages/laptop1.jpg';
 import blog2 from '../img/portImages/graduate.jpg';
-import blog3 from '../img/blogs/blog3.svg';
-import blog4 from '../img/blogs/blog4.svg';
 
 const blogs = [
   {
@@ -16,18 +14,7 @@ const blogs = [
     title: 'Lighthouse Labs Graduation',
     link: 'https://www.linkedin.com/posts/chrisjkang_i-am-excited-to-announce-that-i-have-received-activity-6783067788878630912-DeMV',
   },
-  {
-    id: 3,
-    image: blog3,
-    title: 'Coming Soon!',
-    link: '',
-  },
-  {
-    id: 4,
-    image: blog4,
-    title: 'Coming Soon!',
-    link: '',
-  },
+ 
 ];
 
 export default blogs;
