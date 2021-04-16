5eee41f732e732d6045c7ffbadb67548d48408dc
chriskang24 <christopherjkang24@gmail.com>
2021-04-16 15:45:30 -0400

    fix errors on terminal

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -12,7 +12,7 @@ export default function ContactPage() {
       </div>
       <div className="ContactPage">
         <div className="map-sect">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d369106.72666663735!2d-79.65823226748859!3d43.717899031135076!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d4cb90d7c63ba5%3A0x323555502ab4c477!2sToronto%2C%20ON!5e0!3m2!1sen!2sca!4v1618505108833!5m2!1sen!2sca" width="600" height="450" style={{ border: 0 }} allowfullScreen="" loading="lazy"></iframe>
+          <iframe title={'Current Location'} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d369106.72666663735!2d-79.65823226748859!3d43.717899031135076!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d4cb90d7c63ba5%3A0x323555502ab4c477!2sToronto%2C%20ON!5e0!3m2!1sen!2sca!4v1618505108833!5m2!1sen!2sca" width="600" height="450" style={{ border: 0 }} allowfullScreen="" loading="lazy"></iframe>
         </div>
 
         <div className="contact-sec">
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { faFacebook, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link } from 'react-router-dom';
 
